3b36bdbc89613c0c3c6a8de882bd4ab856bbbf2f
Cyril David <cdhinho27370@outlook.fr>
2026-03-09 17:05:20 +0100

    ticket3 : création du router, du controller, de la page index.phtml et du header/menu

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,29 @@
 <?php
 
+use Cine\App\Controller\MovieController;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__.'/../.env.php';
 
+if(isset($_GET['route'])) {
+    $route = $_GET['route'];
+} else {
+    $route ='index';
+}
 
 
+$movieController = new MovieController;
+
+if ($route === 'index') {
+  $movieController->index();
+} elseif ($route === 'show') {
+  $movieController->show();
+} elseif ($route === 'update') {
+  $movieController->update();
+} elseif ($route === 'delete') {
+  $movieController->delete();
+} elseif ($route === 'search') {
+  $movieController->search();
+}elseif ($route === 'showTmdb') {
+  $movieController->showTmdb();
+}
---
+++ app/src/Controller/MovieController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Cine\app\Controller;
+
+
+class MovieController
+{
+  private $genreRepository;
+  private $filmRepository;
+
+  public function __construct()
+  {
+    
+  }
+  
+  public function index() 
+  {
+    
+  }
+
+  public function show()
+  {
+
+  }
+
+  public function update()
+  {
+
+  }
+
+  public function delete()
+  {
+
+  }
+  
+  public function search()
+  {
+
+  }
+
+  public function showTmdb()
+  {
+    
+  }
+
+}
+
